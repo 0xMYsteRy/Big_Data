858120a796c174973f1de31e83879900710e9090
mystery <s3777242@rmit.edu.vn>
2020-07-26 01:36:59 +0700

    A

---
+++ Week2/tutorial.py
@@ -0,0 +1,20 @@
+# /*
+# 	What other data do you recommend they collect?
+# 	How could value be created?
+# 	How would you measure success?
+# */
+# Walmart / Kmart
+#
+# Kmart special: 50% off
+#
+# Recommend data to collect:
+# +Gender male/ females to buy make up items
+# +Frequence buyers.
+# +Items bought.
+# +Space vs profitable items.
+#
+#
+# +How would you measure success?
+# KPI. Sales & profitibility.
+# How to prove that was ur idea. Company often ignore.
+print("Ok")
